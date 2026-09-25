ca85ee70e1f4b181f5edac13d40d064668f2d57b
luiz henrique-IA <luizhenriquefacudade09@gmail.com>
2026-09-24 21:27:16 -0300

    Configura deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Pet-Center/',
   plugins: [react(), tailwindcss()],
-  base: '/pet-center/',
 })
